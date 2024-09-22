427a7aca324456db88d1b93fd35bd7e16d240d81
Akshay Maldhure <akshaymaldhure@gmail.com>
2024-09-22 22:47:36 +0800

    Reduce implicit and explicit waits

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -18,7 +18,7 @@ public class BasePage {
         LOG.info("Setting up Selenium WebDriver and configuring timeouts");
         this.driver = driver;
         this.driver.manage().window().maximize();
-        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // implicit wait, applies to every element to be searched for
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // explicit wait, applies to a given element conditionally
+        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); // implicit wait, applies to every element to be searched for
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5)); // explicit wait, applies to a given element conditionally
     }
 }
